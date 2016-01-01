48a5f034ffc50a208488f09ea42abcf71047e8f4
Alexander Seliverstov <a.g.seliverstov@yandex.ru>
2016-01-01 14:33:32 +0300

    Upgrade Db version

--- app/src/main/java/it/jaschke/alexandria/data/DbHelper.java
+++ app/src/main/java/it/jaschke/alexandria/data/DbHelper.java
@@ -10,7 +10,7 @@ import android.util.Log;
  */
 public class DbHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "alexandria.db";
 
     public DbHelper(Context context) {
